46037a6f9ef195cdb3cc7093453dae27f8d2116c
Colin Johnston <crj871@gmail.com>
2014-04-14 19:18:42 +1000

    solutions to 1.07

--- Lesson_1_Problem_Set/07-Find_1/hints.py
+++ Lesson_1_Problem_Set/07-Find_1/hints.py
@@ -4,3 +4,6 @@
 # We do NOT accept pull requests that have deleted another contributer's hint or solution without a very clear reason
 # ONLY use concepts covered in the class so far
 # Do NOT give solutions here - ONLY hints
+
+
+# Think about possible python built in methods available
--- Lesson_1_Problem_Set/07-Find_1/solutions.py
+++ Lesson_1_Problem_Set/07-Find_1/solutions.py
@@ -5,3 +5,14 @@
 # ALL solutions must be clearly documented
 # ALL solutions must actually work
 # ONLY use concepts covered in the class so far
+
+text = "first hoo" 
+
+# the 'find' method has two inputs. 
+# the first is the source data
+# the second is the item you are looking for the position
+# The output is a number for the position of the first occurence of the item
+# the first position of a string is 0
+text.find('hoo')
+
+
